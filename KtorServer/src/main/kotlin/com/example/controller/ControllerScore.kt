4cb96c91b0ac9f84bbd6fcc39c4aa735a07c964d
Max <maximilian.fehrentz@envidual.com>
2021-05-12 16:11:52 +0200

    resolved merge conflict, started removing raw sql, postUsers returns User instead of just the id now

--- KtorServer/src/main/kotlin/com/example/controller/ControllerScore.kt
+++ KtorServer/src/main/kotlin/com/example/controller/ControllerScore.kt
@@ -7,45 +7,6 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class ControllerScore {
-//    fun getScores(userIds: Iterable<Int>, exerciseId: Int, timestamp: Long, highscore: Boolean): List<Score> {
-//        val scores = arrayListOf<Score>()
-//        for (userId in userIds) {
-//            val middlePartOfSQLStatement = " FROM exercise_scores " +
-//                    "WHERE user_id = $userId AND exercise = $exerciseId AND timestamp > $timestamp "
-//            var query: String
-//            if (highscore) {
-//                query = "SELECT user_id, exercise, timestamp, max(score) AS score" +
-//                        middlePartOfSQLStatement +
-//                        "GROUP BY user_id, exercise, timestamp;"
-//            } else {
-//                query = "SELECT user_id, exercise, timestamp, score " +
-//                        middlePartOfSQLStatement +
-//                        ";"
-//            }
-//
-//            transaction {
-//                query.execAndReturn { rs ->
-//                    while (rs.next()) {
-//                        val score = Score(
-//                            rs.getInt("user_id"),
-//                            rs.getInt("exercise"),
-//                            rs.getLong("timestamp"),
-//                            rs.getInt("score")
-//                        )
-//                        scores.add(score)
-//                    }
-//                }
-//            }
-//        }
-//
-//
-//        // TODO: remove later, just for debugging purposes
-//        if (highscore && scores.size > 1) {
-//            throw RuntimeException("There cannot be more than one highscore!")
-//        }
-//        return scores
-//    }
-
 
     fun getScores(userIds: Iterable<Int>, exerciseId: Int, timestamp: Long, highscore: Boolean): List<Score> {
         val scores = arrayListOf<Score>()
@@ -84,13 +45,6 @@ class ControllerScore {
         return scores
     }
 
-
-    // TODO: deal with idempotence, don't trash database -> why???
-//    fun putScores(score: Score) {
-//        transaction {
-//            "INSERT INTO exercise_scores (user_id, timestamp, exercise, score) VALUES (${score.userId}, ${score.timestamp}, ${score.exerciseId}, ${score.score});".exec()
-//        }
-//    }
 
     fun putScores(inputScore: Score) {
         transaction {
